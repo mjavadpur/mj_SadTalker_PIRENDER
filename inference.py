e361fada4bfdfd901e63afb9088f632be9753a74
Mohammad Javadpur <mjavadpur@gmail.com>
2023-11-24 16:45:07 +0330

    create inference and update quick_demo

--- inference.py
+++ inference.py
@@ -1,165 +1,199 @@
-from glob import glob
-import shutil
-import torch
-from time import  strftime
-import os, sys, time
 from argparse import ArgumentParser
-
+import time
+import torch, uuid
+import os, sys, shutil, platform
+from src.facerender.pirender_animate import AnimateFromCoeff_PIRender
 from src.utils.preprocess import CropAndExtract
 from src.test_audio2coeff import Audio2Coeff  
 from src.facerender.animate import AnimateFromCoeff
 from src.generate_batch import get_data
 from src.generate_facerender_batch import get_facerender_data
+
 from src.utils.init_path import init_path
 
-def main(args):
-    #torch.backends.cudnn.enabled = False
-
-    inference_startTime = time.time()
-    pic_path = args.source_image
-    audio_path = args.driven_audio
-    save_dir = os.path.join(args.result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
-    os.makedirs(save_dir, exist_ok=True)
-    pose_style = args.pose_style
-    device = args.device
-    batch_size = args.batch_size
-    input_yaw_list = args.input_yaw
-    input_pitch_list = args.input_pitch
-    input_roll_list = args.input_roll
-    ref_eyeblink = args.ref_eyeblink
-    ref_pose = args.ref_pose
-
-    current_root_path = os.path.split(sys.argv[0])[0]
-
-    init_path_startTime = time.time()
-    
-    sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
-    
-    init_path_endTime = time.time()    
-    execution_time = init_path_endTime - init_path_startTime
-    print(f"init_path Execution Time: {execution_time} seconds")
-
-    #init model
-    CropAndExtract_startTime = time.time()
-    preprocess_model = CropAndExtract(sadtalker_paths, device)
-    CropAndExtract_endTime = time.time()    
-    execution_time = CropAndExtract_endTime - CropAndExtract_startTime
-    print(f"CropAndExtract Execution Time: {execution_time} seconds")
+from pydub import AudioSegment
 
 
-    Audio2Coeff_startTime = time.time()
-    audio_to_coeff = Audio2Coeff(sadtalker_paths,  device)
-    Audio2Coeff_endTime = time.time()    
-    execution_time = Audio2Coeff_endTime - Audio2Coeff_startTime
-    print(f"Audio2Coeff Execution Time: {execution_time} seconds")
-    
-    AnimateFromCoeff_startTime = time.time()
-    animate_from_coeff = AnimateFromCoeff(sadtalker_paths, device)
-    AnimateFromCoeff_endTime = time.time()    
-    execution_time = AnimateFromCoeff_endTime - AnimateFromCoeff_startTime
-    print(f"AnimateFromCoeff Execution Time: {execution_time} seconds")
+def mp3_to_wav(mp3_filename,wav_filename,frame_rate):
+    mp3_file = AudioSegment.from_file(file=mp3_filename)
+    mp3_file.set_frame_rate(frame_rate).export(wav_filename,format="wav")
 
-    #crop image and extract 3dmm from image
-    first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
-    os.makedirs(first_frame_dir, exist_ok=True)
-    print('3DMM Extraction for source image')
-    generate_startTime = time.time()
-    first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
-                                                                             source_image_flag=True, pic_size=args.size)
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"CropAndExtract preprocess_model.generate Execution Time: {execution_time} seconds")
-    
-    
-    if first_coeff_path is None:
-        print("Can't get the coeffs of the input")
-        return
 
-    generate_startTime = time.time()
-    if ref_eyeblink is not None:
-        ref_eyeblink_videoname = os.path.splitext(os.path.split(ref_eyeblink)[-1])[0]
-        ref_eyeblink_frame_dir = os.path.join(save_dir, ref_eyeblink_videoname)
-        os.makedirs(ref_eyeblink_frame_dir, exist_ok=True)
-        print('3DMM Extraction for the reference video providing eye blinking')
-        ref_eyeblink_coeff_path, _, _ =  preprocess_model.generate(ref_eyeblink, ref_eyeblink_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_eyeblink_coeff_path=None
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"CropAndExtract ref_eyeblink preprocess_model.generate Execution Time: {execution_time} seconds")
+class SadTalker():
 
-    generate_startTime = time.time()
-    if ref_pose is not None:
-        if ref_pose == ref_eyeblink: 
-            ref_pose_coeff_path = ref_eyeblink_coeff_path
+    def __init__(args, checkpoint_path='checkpoints', config_path='src/config', lazy_load=False):
+
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif platform.system() == 'Darwin': # macos 
+            device = "mps"
         else:
-            ref_pose_videoname = os.path.splitext(os.path.split(ref_pose)[-1])[0]
-            ref_pose_frame_dir = os.path.join(save_dir, ref_pose_videoname)
-            os.makedirs(ref_pose_frame_dir, exist_ok=True)
-            print('3DMM Extraction for the reference video providing pose')
-            ref_pose_coeff_path, _, _ =  preprocess_model.generate(ref_pose, ref_pose_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_pose_coeff_path=None
-
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"CropAndExtract ref_pose preprocess_model.generate Execution Time: {execution_time} seconds")
-    #audio2ceoff
-    generate_startTime = time.time()
-    batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=args.still)
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"get_data Execution Time: {execution_time} seconds")
-    
-    
-    generate_startTime = time.time()
-    coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"audio_to_coeff.generate Execution Time: {execution_time} seconds")
-
-    # 3dface render
-    generate_startTime = time.time()
-    if args.face3dvis:
-        from src.face3d.visualize import gen_composed_video
-        gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"gen_composed_video Execution Time: {execution_time} seconds")
-    
-    #coeff2video
-    generate_startTime = time.time()
-    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
-                                batch_size, input_yaw_list, input_pitch_list, input_roll_list,
-                                expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size)
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"get_facerender_data Execution Time: {execution_time} seconds")
-    
-    
-    generate_startTime = time.time()
-    result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info, \
-                                enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"animate_from_coeff.generate Execution Time: {execution_time} seconds")
-    
-    
-    generate_startTime = time.time()
-    shutil.move(result, save_dir+'.mp4')
-    generate_endTime = time.time()    
-    execution_time = generate_endTime - generate_startTime
-    print(f"shutil.move Execution Time: {execution_time} seconds")
-    
-    print('The generated video is named:', save_dir+'.mp4')
-
-    if not args.verbose:
-        shutil.rmtree(save_dir)
+            device = "cpu"
         
-    inference_endTime = time.time()
+        args.device = device
+
+        os.environ['TORCH_HOME']= checkpoint_path
+
+        args.checkpoint_path = checkpoint_path
+        args.config_path = config_path
+      
+
+    def main(args):
+        # , source_image, driven_audio, preprocess='crop', 
+        #     still_mode=False,  use_enhancer=False, batch_size=1, size=256, 
+        #     pose_style = 0, 
+        #     facerender='facevid2vid',
+        #     exp_scale=1.0, 
+        #     use_ref_video = False,
+        #     ref_video = None,
+        #     ref_info = None,
+        #     use_idle_mode = False,
+        #     length_of_audio = 0, use_blink=True,
+        #     result_dir='./results/'):
+        inference_startTime = time.time()
+        pic_path = args.source_image
+        audio_path = args.driven_audio
+        save_dir = os.path.join(args.result_dir, time.strftime("%Y_%m_%d_%H.%M.%S"))
+        os.makedirs(save_dir, exist_ok=True)
+        pose_style = args.pose_style
+        device = args.device
+        batch_size = args.batch_size
+        input_yaw_list = args.input_yaw
+        input_pitch_list = args.input_pitch
+        input_roll_list = args.input_roll
+        ref_eyeblink = args.ref_eyeblink
+        ref_pose = args.ref_pose
+        use_idle_mode = args.use_idle_mode
+        length_of_audio = args.length_of_audio
+        use_ref_video = args.use_ref_video
+        ref_info = args.ref_info
+        ref_video = args.ref_video
+        
+        current_root_path = os.path.split(sys.argv[0])[0]
+
+        init_path_startTime = time.time()
     
-    execution_time = inference_endTime - inference_startTime
-    print(f"Inference Execution Time: {execution_time} seconds")
+        sadtalker_paths = init_path(args.checkpoint_path, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
+        print(sadtalker_paths)
+            
+        audio_to_coeff = Audio2Coeff(sadtalker_paths, args.device)
+        preprocess_model = CropAndExtract(sadtalker_paths, args.device)
+        
+        if facerender == 'facevid2vid' and args.device != 'mps':
+            animate_from_coeff = AnimateFromCoeff(sadtalker_paths, args.device)
+        elif facerender == 'pirender' or args.device == 'mps':
+            animate_from_coeff = AnimateFromCoeff_PIRender(sadtalker_paths, args.device)
+            facerender = 'pirender'
+        else:
+            raise(RuntimeError('Unknown model: {}'.format(facerender)))
+            
+
+        input_dir = os.path.join(save_dir, 'input')
+        os.makedirs(input_dir, exist_ok=True)
+
+        print(pic_path)
+        pic_path = os.path.join(input_dir, os.path.basename(pic_path)) 
+        shutil.move(pic_path, input_dir)
+
+        if audio_path is not None and os.path.isfile(audio_path):
+            audio_path = os.path.join(input_dir, os.path.basename(audio_path))  
+
+            #### mp3 to wav
+            if '.mp3' in audio_path:
+                mp3_to_wav(audio_path, audio_path.replace('.mp3', '.wav'), 16000)
+                audio_path = audio_path.replace('.mp3', '.wav')
+            else:
+                shutil.move(audio_path, input_dir)
+
+        elif use_idle_mode:
+            audio_path = os.path.join(input_dir, 'idlemode_'+str(length_of_audio)+'.wav') ## generate audio from this new audio_path
+            from pydub import AudioSegment
+            one_sec_segment = AudioSegment.silent(duration=1000*length_of_audio)  #duration in milliseconds
+            one_sec_segment.export(audio_path, format="wav")
+        else:
+            print(use_ref_video, ref_info)
+            assert use_ref_video == True and ref_info == 'all'
+
+        if use_ref_video and ref_info == 'all': # full ref mode
+            ref_video_videoname = os.path.basename(ref_video)
+            audio_path = os.path.join(save_dir, ref_video_videoname+'.wav')
+            print('new audiopath:',audio_path)
+            # if ref_video contains audio, set the audio from ref_video.
+            cmd = r"ffmpeg -y -hide_banner -loglevel error -i %s %s"%(ref_video, audio_path)
+            os.system(cmd)        
+
+        os.makedirs(save_dir, exist_ok=True)
+        
+        #crop image and extract 3dmm from image
+        first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
+        os.makedirs(first_frame_dir, exist_ok=True)
+        first_coeff_path, crop_pic_path, crop_info = preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
+                                                                             source_image_flag=True, pic_size=args.size)
+        
+        if first_coeff_path is None:
+            raise AttributeError("No face is detected")
+
+        if use_ref_video:
+            print('using ref video for genreation')
+            ref_video_videoname = os.path.splitext(os.path.split(ref_video)[-1])[0]
+            ref_video_frame_dir = os.path.join(save_dir, ref_video_videoname)
+            os.makedirs(ref_video_frame_dir, exist_ok=True)
+            print('3DMM Extraction for the reference video providing pose')
+            ref_video_coeff_path, _, _ =  preprocess_model.generate(ref_video, ref_video_frame_dir, args.preprocess,\
+                                                                             source_image_flag=True)
+        else:
+            ref_video_coeff_path = None
+
+        if use_ref_video:
+            if ref_info == 'pose':
+                ref_pose_coeff_path = ref_video_coeff_path
+                ref_eyeblink_coeff_path = None
+            elif ref_info == 'blink':
+                ref_pose_coeff_path = None
+                ref_eyeblink_coeff_path = ref_video_coeff_path
+            elif ref_info == 'pose+blink':
+                ref_pose_coeff_path = ref_video_coeff_path
+                ref_eyeblink_coeff_path = ref_video_coeff_path
+            elif ref_info == 'all':            
+                ref_pose_coeff_path = None
+                ref_eyeblink_coeff_path = None
+            else:
+                raise('error in refinfo')
+        else:
+            ref_pose_coeff_path = None
+            ref_eyeblink_coeff_path = None
+
+        #audio2ceoff
+        if use_ref_video and ref_info == 'all':
+            coeff_path = ref_video_coeff_path # audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+        else:
+            batch = get_data(first_coeff_path, audio_path, args.device, ref_eyeblink_coeff_path=ref_eyeblink_coeff_path, still=args.still, \
+                idlemode=use_idle_mode, length_of_audio=length_of_audio, use_blink=args.use_blink) # longer audio?
+            coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+
+        #coeff2video
+        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=args.still, \
+            preprocess=args.preprocess, size=args.size, expression_scale = args.expression_scale, facemodel=facerender)
+        return_path = animate_from_coeff.generate(data, save_dir,  pic_path, crop_info, enhancer=args.enhancer, preprocess=args.preprocess, img_size=args.size)
+        video_name = data['video_name']
+        print(f'The generated video is named {video_name} in {save_dir}')
+        shutil.move(return_path, save_dir+'.mp4')
+
+        del preprocess_model
+        del audio_to_coeff
+        del animate_from_coeff
+
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+            torch.cuda.synchronize()
+            
+        import gc; gc.collect()
+        
+        
+        inference_endTime = time.time()
+        
+        execution_time = inference_endTime - inference_startTime
+        print(f"Inference Execution Time: {execution_time} seconds")
 
     
 if __name__ == '__main__':
@@ -186,7 +220,13 @@ if __name__ == '__main__':
     parser.add_argument("--preprocess", default='crop', choices=['crop', 'extcrop', 'resize', 'full', 'extfull'], help="how to preprocess the images" ) 
     parser.add_argument("--verbose",action="store_true", help="saving the intermedia output or not" ) 
     parser.add_argument("--old_version",action="store_true", help="use the pth other than safetensor version" ) 
-
+    parser.add_argument("--use_idle_mode",action="store_false", help="use_idle_mode" ) 
+    parser.add_argument("--length_of_audio", type=int, default=0,  help="length_of_audio")
+    parser.add_argument("--use_ref_video",action="store_false", help="use_ref_video" ) 
+    parser.add_argument("--ref_info", default=None, choices=['pose', 'blink', 'pose+blink', 'all'], help="" ) 
+    parser.add_argument("--ref_video", default=None, help="ref_video")
+    parser.add_argument("--use_blink",action="store_true", help="use_blink" ) 
+ 
 
     # net structure and parameters
     parser.add_argument('--net_recon', type=str, default='resnet50', choices=['resnet18', 'resnet34', 'resnet50'], help='useless')
@@ -211,3 +251,4 @@ if __name__ == '__main__':
 
     main(args)
 
+  
