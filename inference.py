a86e312e6bcc1142dc3332daa8327d8861a4b0ca
Mohammad Javadpur <mjavadpur@gmail.com>
2023-11-24 18:49:52 +0330

    fixed sum buges and add pirender model download

--- inference.py
+++ inference.py
@@ -19,183 +19,171 @@ def mp3_to_wav(mp3_filename,wav_filename,frame_rate):
     mp3_file.set_frame_rate(frame_rate).export(wav_filename,format="wav")
 
 
-class SadTalker():
 
-    def __init__(args, checkpoint_path='checkpoints', config_path='src/config', lazy_load=False):
 
-        if torch.cuda.is_available():
-            device = "cuda"
-        elif platform.system() == 'Darwin': # macos 
-            device = "mps"
-        else:
-            device = "cpu"
-        
-        args.device = device
-
-        os.environ['TORCH_HOME']= checkpoint_path
-
-        args.checkpoint_path = checkpoint_path
-        args.config_path = config_path
-      
-
-    def main(args):
-        # , source_image, driven_audio, preprocess='crop', 
-        #     still_mode=False,  use_enhancer=False, batch_size=1, size=256, 
-        #     pose_style = 0, 
-        #     facerender='facevid2vid',
-        #     exp_scale=1.0, 
-        #     use_ref_video = False,
-        #     ref_video = None,
-        #     ref_info = None,
-        #     use_idle_mode = False,
-        #     length_of_audio = 0, use_blink=True,
-        #     result_dir='./results/'):
-        inference_startTime = time.time()
-        pic_path = args.source_image
-        audio_path = args.driven_audio
-        save_dir = os.path.join(args.result_dir, time.strftime("%Y_%m_%d_%H.%M.%S"))
-        os.makedirs(save_dir, exist_ok=True)
-        pose_style = args.pose_style
-        device = args.device
-        batch_size = args.batch_size
-        input_yaw_list = args.input_yaw
-        input_pitch_list = args.input_pitch
-        input_roll_list = args.input_roll
-        ref_eyeblink = args.ref_eyeblink
-        ref_pose = args.ref_pose
-        use_idle_mode = args.use_idle_mode
-        length_of_audio = args.length_of_audio
-        use_ref_video = args.use_ref_video
-        ref_info = args.ref_info
-        ref_video = args.ref_video
-        
-        current_root_path = os.path.split(sys.argv[0])[0]
-
-        init_path_startTime = time.time()
+def main(args):
+    # , source_image, driven_audio, preprocess='crop', 
+    #     still_mode=False,  use_enhancer=False, batch_size=1, size=256, 
+    #     pose_style = 0, 
+    #     facerender='facevid2vid',
+    #     exp_scale=1.0, 
+    #     use_ref_video = False,
+    #     ref_video = None,
+    #     ref_info = None,
+    #     use_idle_mode = False,
+    #     length_of_audio = 0, use_blink=True,
+    #     result_dir='./results/'):
+    inference_startTime = time.time()
+    pic_path = args.source_image
+    audio_path = args.driven_audio
+    save_dir = os.path.join(args.result_dir, time.strftime("%Y_%m_%d_%H.%M.%S"))
+    os.makedirs(save_dir, exist_ok=True)
+    pose_style = args.pose_style
+    device = args.device
+    batch_size = args.batch_size
+    input_yaw_list = args.input_yaw
+    input_pitch_list = args.input_pitch
+    input_roll_list = args.input_roll
+    ref_eyeblink = args.ref_eyeblink
+    ref_pose = args.ref_pose
+    use_idle_mode = args.use_idle_mode
+    length_of_audio = args.length_of_audio
+    use_ref_video = False # args.use_ref_video
+    ref_info = args.ref_info
+    ref_video = args.ref_video
+    facerender = args.facerender
     
-        sadtalker_paths = init_path(args.checkpoint_path, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
-        print(sadtalker_paths)
-            
-        audio_to_coeff = Audio2Coeff(sadtalker_paths, args.device)
-        preprocess_model = CropAndExtract(sadtalker_paths, args.device)
+    current_root_path = os.path.split(sys.argv[0])[0]
+    os.environ['TORCH_HOME']= args.checkpoint_dir
+    config_path = 'src/config'
+
+    init_path_startTime = time.time()
+
+    sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, config_path), args.size, args.old_version, args.preprocess)
+    print(sadtalker_paths)
         
-        if facerender == 'facevid2vid' and args.device != 'mps':
-            animate_from_coeff = AnimateFromCoeff(sadtalker_paths, args.device)
-        elif facerender == 'pirender' or args.device == 'mps':
-            animate_from_coeff = AnimateFromCoeff_PIRender(sadtalker_paths, args.device)
-            facerender = 'pirender'
-        else:
-            raise(RuntimeError('Unknown model: {}'.format(facerender)))
-            
-
-        input_dir = os.path.join(save_dir, 'input')
-        os.makedirs(input_dir, exist_ok=True)
-
-        print(pic_path)
-        pic_path = os.path.join(input_dir, os.path.basename(pic_path)) 
-        shutil.move(pic_path, input_dir)
-
-        if audio_path is not None and os.path.isfile(audio_path):
-            audio_path = os.path.join(input_dir, os.path.basename(audio_path))  
-
-            #### mp3 to wav
-            if '.mp3' in audio_path:
-                mp3_to_wav(audio_path, audio_path.replace('.mp3', '.wav'), 16000)
-                audio_path = audio_path.replace('.mp3', '.wav')
-            else:
-                shutil.move(audio_path, input_dir)
-
-        elif use_idle_mode:
-            audio_path = os.path.join(input_dir, 'idlemode_'+str(length_of_audio)+'.wav') ## generate audio from this new audio_path
-            from pydub import AudioSegment
-            one_sec_segment = AudioSegment.silent(duration=1000*length_of_audio)  #duration in milliseconds
-            one_sec_segment.export(audio_path, format="wav")
-        else:
-            print(use_ref_video, ref_info)
-            assert use_ref_video == True and ref_info == 'all'
-
-        if use_ref_video and ref_info == 'all': # full ref mode
-            ref_video_videoname = os.path.basename(ref_video)
-            audio_path = os.path.join(save_dir, ref_video_videoname+'.wav')
-            print('new audiopath:',audio_path)
-            # if ref_video contains audio, set the audio from ref_video.
-            cmd = r"ffmpeg -y -hide_banner -loglevel error -i %s %s"%(ref_video, audio_path)
-            os.system(cmd)        
-
-        os.makedirs(save_dir, exist_ok=True)
+    audio_to_coeff = Audio2Coeff(sadtalker_paths, args.device)
+    preprocess_model = CropAndExtract(sadtalker_paths, args.device)
+    
+    if facerender == 'facevid2vid' and args.device != 'mps':
+        animate_from_coeff = AnimateFromCoeff(sadtalker_paths, args.device)
+    elif facerender == 'pirender' or args.device == 'mps':
+        animate_from_coeff = AnimateFromCoeff_PIRender(sadtalker_paths, args.device)
+        facerender = 'pirender'
+    else:
+        raise(RuntimeError('Unknown model: {}'.format(facerender)))
         
-        #crop image and extract 3dmm from image
-        first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
-        os.makedirs(first_frame_dir, exist_ok=True)
-        first_coeff_path, crop_pic_path, crop_info = preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
-                                                                             source_image_flag=True, pic_size=args.size)
-        
-        if first_coeff_path is None:
-            raise AttributeError("No face is detected")
 
-        if use_ref_video:
-            print('using ref video for genreation')
-            ref_video_videoname = os.path.splitext(os.path.split(ref_video)[-1])[0]
-            ref_video_frame_dir = os.path.join(save_dir, ref_video_videoname)
-            os.makedirs(ref_video_frame_dir, exist_ok=True)
-            print('3DMM Extraction for the reference video providing pose')
-            ref_video_coeff_path, _, _ =  preprocess_model.generate(ref_video, ref_video_frame_dir, args.preprocess,\
-                                                                             source_image_flag=True)
-        else:
-            ref_video_coeff_path = None
+    print("save_dir:" + save_dir)
+    input_dir = os.path.join(save_dir, 'input')
+    os.makedirs(input_dir, exist_ok=True)
 
-        if use_ref_video:
-            if ref_info == 'pose':
-                ref_pose_coeff_path = ref_video_coeff_path
-                ref_eyeblink_coeff_path = None
-            elif ref_info == 'blink':
-                ref_pose_coeff_path = None
-                ref_eyeblink_coeff_path = ref_video_coeff_path
-            elif ref_info == 'pose+blink':
-                ref_pose_coeff_path = ref_video_coeff_path
-                ref_eyeblink_coeff_path = ref_video_coeff_path
-            elif ref_info == 'all':            
-                ref_pose_coeff_path = None
-                ref_eyeblink_coeff_path = None
-            else:
-                raise('error in refinfo')
+    print("input_dir:" + input_dir)
+    print("pic_path:" + pic_path)
+    # pic_path = os.path.join(input_dir, os.path.basename(pic_path)) 
+    # shutil.move(pic_path, input_dir)
+
+    if audio_path is not None and os.path.isfile(audio_path):
+        # audio_path = os.path.join(input_dir, os.path.basename(audio_path))  
+
+        #### mp3 to wav
+        if '.mp3' in audio_path:
+            mp3_to_wav(audio_path, audio_path.replace('.mp3', '.wav'), 16000)
+            audio_path = audio_path.replace('.mp3', '.wav')
         else:
+            print("audio_path:"+audio_path)
+            shutil.move(audio_path, input_dir)
+
+    elif use_idle_mode:
+        audio_path = os.path.join(input_dir, 'idlemode_'+str(length_of_audio)+'.wav') ## generate audio from this new audio_path
+        from pydub import AudioSegment
+        one_sec_segment = AudioSegment.silent(duration=1000*length_of_audio)  #duration in milliseconds
+        one_sec_segment.export(audio_path, format="wav")
+    else:
+        print(use_ref_video, ref_info)
+        assert use_ref_video == True and ref_info == 'all'
+
+    if use_ref_video and ref_info == 'all': # full ref mode
+        ref_video_videoname = os.path.basename(ref_video)
+        audio_path = os.path.join(save_dir, ref_video_videoname+'.wav')
+        print('new audiopath:',audio_path)
+        # if ref_video contains audio, set the audio from ref_video.
+        cmd = r"ffmpeg -y -hide_banner -loglevel error -i %s %s"%(ref_video, audio_path)
+        os.system(cmd)        
+
+    os.makedirs(save_dir, exist_ok=True)
+    
+    #crop image and extract 3dmm from image
+    first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
+    os.makedirs(first_frame_dir, exist_ok=True)
+    first_coeff_path, crop_pic_path, crop_info = preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
+                                                                            source_image_flag=True, pic_size=args.size)
+    
+    if first_coeff_path is None:
+        raise AttributeError("No face is detected")
+
+    if use_ref_video:
+        print('using ref video for genreation')
+        ref_video_videoname = os.path.splitext(os.path.split(ref_video)[-1])[0]
+        ref_video_frame_dir = os.path.join(save_dir, ref_video_videoname)
+        os.makedirs(ref_video_frame_dir, exist_ok=True)
+        print('3DMM Extraction for the reference video providing pose')
+        ref_video_coeff_path, _, _ =  preprocess_model.generate(ref_video, ref_video_frame_dir, args.preprocess,\
+                                                                            source_image_flag=True)
+    else:
+        ref_video_coeff_path = None
+
+    if use_ref_video:
+        if ref_info == 'pose':
+            ref_pose_coeff_path = ref_video_coeff_path
+            ref_eyeblink_coeff_path = None
+        elif ref_info == 'blink':
+            ref_pose_coeff_path = None
+            ref_eyeblink_coeff_path = ref_video_coeff_path
+        elif ref_info == 'pose+blink':
+            ref_pose_coeff_path = ref_video_coeff_path
+            ref_eyeblink_coeff_path = ref_video_coeff_path
+        elif ref_info == 'all':            
             ref_pose_coeff_path = None
             ref_eyeblink_coeff_path = None
-
-        #audio2ceoff
-        if use_ref_video and ref_info == 'all':
-            coeff_path = ref_video_coeff_path # audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
         else:
-            batch = get_data(first_coeff_path, audio_path, args.device, ref_eyeblink_coeff_path=ref_eyeblink_coeff_path, still=args.still, \
-                idlemode=use_idle_mode, length_of_audio=length_of_audio, use_blink=args.use_blink) # longer audio?
-            coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+            raise('error in refinfo')
+    else:
+        ref_pose_coeff_path = None
+        ref_eyeblink_coeff_path = None
 
-        #coeff2video
-        data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=args.still, \
-            preprocess=args.preprocess, size=args.size, expression_scale = args.expression_scale, facemodel=facerender)
-        return_path = animate_from_coeff.generate(data, save_dir,  pic_path, crop_info, enhancer=args.enhancer, preprocess=args.preprocess, img_size=args.size)
-        video_name = data['video_name']
-        print(f'The generated video is named {video_name} in {save_dir}')
-        shutil.move(return_path, save_dir+'.mp4')
+    #audio2ceoff
+    if use_ref_video and ref_info == 'all':
+        coeff_path = ref_video_coeff_path # audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+    else:
+        batch = get_data(first_coeff_path, audio_path, args.device, ref_eyeblink_coeff_path=ref_eyeblink_coeff_path, still=args.still, \
+            idlemode=use_idle_mode, length_of_audio=length_of_audio, use_blink=args.use_blink) # longer audio?
+        coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
 
-        del preprocess_model
-        del audio_to_coeff
-        del animate_from_coeff
+    #coeff2video
+    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, batch_size, still_mode=args.still, \
+        preprocess=args.preprocess, size=args.size, expression_scale = args.expression_scale, facemodel=facerender)
+    return_path = animate_from_coeff.generate(data, save_dir,  pic_path, crop_info, enhancer=args.enhancer, preprocess=args.preprocess, img_size=args.size)
+    video_name = data['video_name']
+    print(f'The generated video is named {video_name} in {save_dir}')
+    shutil.move(return_path, save_dir+'.mp4')
 
-        if torch.cuda.is_available():
-            torch.cuda.empty_cache()
-            torch.cuda.synchronize()
-            
-        import gc; gc.collect()
+    del preprocess_model
+    del audio_to_coeff
+    del animate_from_coeff
+
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.synchronize()
         
-        
-        inference_endTime = time.time()
-        
-        execution_time = inference_endTime - inference_startTime
-        print(f"Inference Execution Time: {execution_time} seconds")
-
+    import gc; gc.collect()
     
+    
+    inference_endTime = time.time()
+    
+    execution_time = inference_endTime - inference_startTime
+    print(f"Inference Execution Time: {execution_time} seconds")
+
 if __name__ == '__main__':
 
     parser = ArgumentParser()  
@@ -226,6 +214,7 @@ if __name__ == '__main__':
     parser.add_argument("--ref_info", default=None, choices=['pose', 'blink', 'pose+blink', 'all'], help="" ) 
     parser.add_argument("--ref_video", default=None, help="ref_video")
     parser.add_argument("--use_blink",action="store_true", help="use_blink" ) 
+    parser.add_argument('--facerender',  type=str, default='pirender', help="Face enhancer, [facevid2vid, pirender]")
  
 
     # net structure and parameters
---
+++ scripts/download_models.sh
@@ -0,0 +1,38 @@
+mkdir ./checkpoints  
+
+# lagency download link
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/auido2exp_00300-model.pth -O ./checkpoints/auido2exp_00300-model.pth
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/auido2pose_00140-model.pth -O ./checkpoints/auido2pose_00140-model.pth
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/epoch_20.pth -O ./checkpoints/epoch_20.pth
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/facevid2vid_00189-model.pth.tar -O ./checkpoints/facevid2vid_00189-model.pth.tar
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/shape_predictor_68_face_landmarks.dat -O ./checkpoints/shape_predictor_68_face_landmarks.dat
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/wav2lip.pth -O ./checkpoints/wav2lip.pth
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/mapping_00229-model.pth.tar -O ./checkpoints/mapping_00229-model.pth.tar
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/mapping_00109-model.pth.tar -O ./checkpoints/mapping_00109-model.pth.tar
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/hub.zip -O ./checkpoints/hub.zip
+# unzip -n ./checkpoints/hub.zip -d ./checkpoints/
+
+!gdown https://drive.google.com/uc?id=1-0xOf6g58OmtKtEWJlU3VlnfRqPN9Uq7 
+!unzip -x ./face.zip
+%mv /content/mj_SadTalker_pirender/face/latest_checkpoint.txt /content/mj_SadTalker_pirender/checkpoints
+%mv /content/mj_SadTalker_pirender/face/epoch_00190_iteration_000400000_checkpoint.pt /content/mj_SadTalker_pirender/checkpoints
+# rm face.zip
+
+#### download the new links.
+wget -nc https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00109-model.pth.tar -O  ./checkpoints/mapping_00109-model.pth.tar
+wget -nc https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/mapping_00229-model.pth.tar -O  ./checkpoints/mapping_00229-model.pth.tar
+wget -nc https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/SadTalker_V0.0.2_256.safetensors -O  ./checkpoints/SadTalker_V0.0.2_256.safetensors
+wget -nc https://github.com/OpenTalker/SadTalker/releases/download/v0.0.2-rc/SadTalker_V0.0.2_512.safetensors -O  ./checkpoints/SadTalker_V0.0.2_512.safetensors
+wget -nc https://huggingface.co/vinthony/SadTalker-V002rc/blob/main/epoch_00190_iteration_000400000_checkpoint.pt -O ./checkpoints/epoch_00190_iteration_000400000_checkpoint.pt 
+
+
+# wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/BFM_Fitting.zip -O ./checkpoints/BFM_Fitting.zip
+# unzip -n ./checkpoints/BFM_Fitting.zip -d ./checkpoints/
+
+### enhancer 
+mkdir -p ./gfpgan/weights
+wget -nc https://github.com/xinntao/facexlib/releases/download/v0.1.0/alignment_WFLW_4HG.pth -O ./gfpgan/weights/alignment_WFLW_4HG.pth 
+wget -nc https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth -O ./gfpgan/weights/detection_Resnet50_Final.pth 
+wget -nc https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth -O ./gfpgan/weights/GFPGANv1.4.pth 
+wget -nc https://github.com/xinntao/facexlib/releases/download/v0.2.2/parsing_parsenet.pth -O ./gfpgan/weights/parsing_parsenet.pth 
+
